049aa581573cb4b625d02a645023673b60e5131a
Thursaun <Thursaun.Canton@gmail.com>
2025-06-20 16:28:25 -0400

    Fix missing error utilities and module syntax for Railway deployment

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../utils/errors/unauthorizedError');
+const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 const { JWT_SECRET } = require('../config/config');
 const { ERROR_MESSAGES } = require('../config/constants');
 
